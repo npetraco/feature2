f06112805250f29283b6ff7f4ff9d5c6182085ba
npetraco <npetraco@gmail.com>
2015-08-17 21:29:34 -0400

    cleaned up circle cropping function a bit

--- R/crop.circular.roi.R
+++ R/crop.circular.roi.R
@@ -15,30 +15,27 @@
 #--------------------------------------------
 crop.circular.roi <- function(dmat, xc.idx, yc.idx, pix.radius, crop.empty.border=TRUE, roi.type="inner"){
   
-  
+  #Make mask for circle where you want it:
   mask <- Circular_Mask_ROI(dmat, xc.idx, yc.idx, pix.radius)
+  
   cropped.dmat <- array(NaN, dim(dmat))
   
+  #Keep the inner part
   if(roi.type=="inner") {
-    
-    keep.idxs <- which(mask == 1, arr.ind=TRUE)
-    cropped.dmat[keep.idxs] <- dmat[keep.idxs]
-    
-    if(crop.empty.border==TRUE) {
-      cropped.dmat <- crop.retangular.roi(cropped.dmat, clip.empty.border=TRUE)
-    }
-  }
+    keep.idxs <- which(mask == 1, arr.ind=TRUE)    
+  } 
   
+  #keep what's outside the circle
   if(roi.type=="outer") {
-    
-    keep.idxs <- which(mask == 0, arr.ind=TRUE)
-    cropped.dmat[keep.idxs] <- dmat[keep.idxs]
-    
-    if(crop.empty.border==TRUE) {
-      cropped.dmat <- crop.retangular.roi(cropped.dmat, clip.empty.border=TRUE)
-    }
-  }
+    keep.idxs <- which(mask == 1, arr.ind=TRUE)    
+  } 
   
+  cropped.dmat[keep.idxs] <- dmat[keep.idxs]
+  
+  if(crop.empty.border==TRUE) {
+    cropped.dmat <- crop.retangular.roi(cropped.dmat, clip.empty.border=TRUE)
+  }
+    
   return(cropped.dmat)
 
 }
